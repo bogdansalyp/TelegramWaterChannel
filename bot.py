c10153c24f7859b14a877b2fc801d56ba0565a79
bsalyp <bsalyp@cisco.com>
2019-07-24 21:06:05 +0300

    Extreme code optimisation

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import telegram
 from key import token
 
 bot = telegram.Bot(token=token)
-msg = "папей воды"
-status = bot.send_message(chat_id="@WaterReminder", text=msg, parse_mode=telegram.ParseMode.HTML)
+status = bot.send_message(chat_id="@WaterReminder", text="папей воды", parse_mode=telegram.ParseMode.HTML)
 
 print(status)
